8ba966bd580f04a7d6ecd8e7b46b93d1e382a4a8
Glaydson <glaydsonvs@gmail.com>
2020-08-31 17:15:25 -0300

    Primeiro commit

--- academia-jedi-2020-s2/e2e/src/app.e2e-spec.ts
+++ academia-jedi-2020-s2/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('agp2020-s2 app is running!');
+    expect(page.getTitleText()).toEqual('academia-jedi2020-s2 app is running!');
   });
 
   afterEach(async () => {
