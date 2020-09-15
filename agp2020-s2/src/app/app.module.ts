3d2f352ec9a72e83e9367d496f0f906582a6bf88
Glaydson <glaydsonvs@gmail.comm>
2020-09-14 22:04:45 -0300

    Aula de 14-09-2020

--- agp2020-s2/src/app/app.module.ts
+++ agp2020-s2/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProdutosComponent } from './produtos/lista-produtos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListaProdutosComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
