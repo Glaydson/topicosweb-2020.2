9a9b52dcc292b6ac5a551d1f90d235eab3025522
Glaydson <glaydsonvs@gmail.com>
2020-09-22 17:21:52 -0300

    Exercício 2 - Lista de Estudantes

--- academia-jedi-2020-s2/src/app/app.module.ts
+++ academia-jedi-2020-s2/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEstudantesComponent } from './estudantes/lista-estudantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEstudantesComponent,
   ],
   imports: [
     BrowserModule,
