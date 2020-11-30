70247ab7b0694b37f430b2968629aad2b8d5fb69
Glaydson <glaydsonvs@gmail.com>
2020-11-30 19:25:04 -0300

    Exercício Unidade 8

--- academia-jedi-2020-s2/src/app/estudantes/lista-estudantes.component.ts
+++ academia-jedi-2020-s2/src/app/estudantes/lista-estudantes.component.ts
@@ -3,54 +3,64 @@ import { IEstudante } from './estudante';
 import { EstudantesService } from './estudantes.service';
 
 @Component({
-    selector: "jedi-estudantes",
-    templateUrl: "./lista-estudantes.component.html",
-    styleUrls: ['./lista-estudantes.component.css']
+  selector: "jedi-estudantes",
+  templateUrl: "./lista-estudantes.component.html",
+  styleUrls: ['./lista-estudantes.component.css']
 })
 export class ListaEstudantesComponent implements OnInit {
-    tituloPagina: string = "Lista de Estudantes"
-    larguraImagem: number = 50;
-    margemImagem: number = 2;
-    exibirImagem: boolean = true;
+  tituloPagina: string = "Lista de Estudantes"
+  larguraImagem: number = 50;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
 
-    _filtroLista: string;
-    get filtroLista() : string {
-      return this._filtroLista;
-    }
-    set filtroLista(valor: string) {
-      this._filtroLista = valor;
-      this.estudantesFiltrados = this.filtroLista ? this.executarFiltro(this.filtroLista) : this.estudantes;
-    }
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(valor: string) {
+    this._filtroLista = valor;
+    this.estudantesFiltrados = this.filtroLista ? this.executarFiltro(this.filtroLista) : this.estudantes;
+  }
 
-    estudantesFiltrados: IEstudante[];
+  estudantesFiltrados: IEstudante[];
 
-    alturaMaxima: number;
-    alturasEstudantes: number[];
-    
-    estudantes: IEstudante[];
+  alturaMaxima: number;
+  alturasEstudantes: number[];
 
-    constructor(private estudantesServico: EstudantesService) {
-        
-    }
-      
-    ngOnInit() {
-        
-        this.estudantes = this.estudantesServico.getEstudantes();
+  estudantes: IEstudante[];
+
+  mensagemErro: string;
+
+  constructor(private estudantesServico: EstudantesService) {
+
+  }
+
+  ngOnInit() {
+    this.getEstudantes();
+  }
+
+  getEstudantes(): void {
+    this.estudantesServico.getEstudantes().subscribe(
+      estudantes => {
+        this.estudantes = estudantes;
         this.estudantesFiltrados = this.estudantes;
         this.filtroLista = 'Luke';
         this.alturasEstudantes = this.estudantes.map(e => e.altura);
         this.alturaMaxima = Math.max(...this.alturasEstudantes);
-    }
+      },
+      error => this.mensagemErro = <any>error
+    );
+  }
 
-    alternarImagem(): void {
-        this.exibirImagem = !this.exibirImagem;
-    }
+  alternarImagem(): void {
+    this.exibirImagem = !this.exibirImagem;
+  }
 
-    executarFiltro(filtrarPor: string): IEstudante[] {
-        filtrarPor = filtrarPor.toLocaleLowerCase();
-        return this.estudantes.filter((estudante: IEstudante) => 
-          estudante.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
-    }
-  
-      
+  executarFiltro(filtrarPor: string): IEstudante[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.estudantes.filter((estudante: IEstudante) =>
+      estudante.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1);
+  }
+
+
 }
